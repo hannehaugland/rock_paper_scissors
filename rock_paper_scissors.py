5763e8095e3a9ffc02f0994d3d4d75e743c7ed3d
hannehaugland <hsofiehaugland@gmail.com>
2022-07-06 11:14:32 +0200

    added user input

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,3 +1,11 @@
 import random 
 
-# a simple rock, paper, scissors game with GUI 
+user_wins = 0
+comp_wins = 0 
+
+options = ["rock", "paper", "scissors"]
+users_choice = ""
+
+while users_choice != 'q': 
+    user_input = input("Rock/paper/scissors or Q to quit: ").lower()
+
