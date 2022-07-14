4bf5eb13f2668eb7440bc6e5267113501c1d05fd
hannehaugland <hsofiehaugland@gmail.com>
2022-07-14 09:40:21 +0200

    init

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -9,3 +9,12 @@ users_choice = ""
 while users_choice != 'q': 
     user_input = input("Rock/paper/scissors or Q to quit: ").lower()
 
+    if user_input not in options: 
+        continue
+    
+    #computers choice made by random: rock: 0, paper: 1, scissor: 2 
+    random_number = random.randint(0, 2)
+    computers_choice = options[random_number]
+    print("Computer picked {}.").format(computers_choice)
+
+
