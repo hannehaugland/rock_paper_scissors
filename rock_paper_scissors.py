034db2934ae83aec4ad3385c1a1aa3007866ae6e
hannehaugland <hsofiehaugland@gmail.com>
2022-07-14 10:19:59 +0200

    completion and bug fixes after testing

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,9 +4,9 @@ user_wins = 0
 comp_wins = 0 
 
 options = ["rock", "paper", "scissors"]
-users_choice = ""
+user_input = ""
 
-while users_choice != 'q': 
+while user_input != "q": 
     user_input = input("Rock/paper/scissors or Q to quit: ").lower()
 
     if user_input not in options: 
@@ -15,6 +15,20 @@ while users_choice != 'q':
     #computers choice made by random: rock: 0, paper: 1, scissor: 2 
     random_number = random.randint(0, 2)
     computers_choice = options[random_number]
-    print("Computer picked {}.").format(computers_choice)
+    print("Computer picked {}.".format(computers_choice))
 
+    if user_input == "rock" and computers_choice == "scissors": 
+        user_wins += 1 
+        print("You won!")
+    elif user_input == "paper" and computers_choice == "rock":
+        user_wins += 1
+        print("You won!")
+    elif user_input == "scissors" and computers_choice == "paper": 
+        user_wins += 1
+        print("You won!")
+    else: 
+        comp_wins += 1
+        print("You lost!")
 
+print("\nYou won {} time(s), the computer won {} time(s).".format(user_wins, comp_wins))
+print("Thanks for playing!")
